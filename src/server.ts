83eb7dd6130d4956f0af6315c3fe3da2af734267
Colin <colin.lawrence95@gmail.com>
2025-07-10 15:37:06 -0400

    coors 6

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,29 @@ dotenv.config();
 import express from "express";
 
 import path from "path";
-const cors = require("cors");
+import cors from "cors";
 import reviewsRouter from "./routes/reviews";
 import listingsRouter from "./routes/listings";
 
+const allowedOrigins = ["http://localhost:3000", "https://thejimkellyteam.com"];
+
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                console.log("Blocked by CORS:", origin);
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 app.use(express.json());
 
 app.use("/api/reviews", reviewsRouter);
